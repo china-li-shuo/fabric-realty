1a040efdad2521900ea4f865a90750f769bdedbc
china-li-shuo <676050059@qq.com>
2024-04-29 11:43:11 +0800

    判断活动是否开启

--- application/server/api/v1/seckill.go
+++ application/server/api/v1/seckill.go
@@ -3,15 +3,18 @@ package v1
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 
+	"application/model"
 	"application/pkg/app"
 )
 
 type SeckillGoods struct {
-	GoodID string `json:"good_id"` //操作人ID
-	UserID string `json:"user_id"` //所有者(业主)(业主AccountId)
+	GoodID     string `json:"good_id" form:"good_id"`         //商品ID
+	UserID     string `json:"user_id" form:"user_id"`         //用户ID
+	ActivityID string `json:"activity_id" form:"activity_id"` //活动ID
 }
 
 // SecKill 秒杀
@@ -23,8 +26,23 @@ func SecKill(c *gin.Context) {
 		appG.Response(http.StatusBadRequest, "失败", fmt.Sprintf("参数出错%s", err.Error()))
 		return
 	}
+	//去查询活动的信息
+	var activityData model.Activity
+	tx := model.DB.Model(&model.Activity{}).First(&activityData, body.ActivityID)
+	if tx.RowsAffected == 0 {
+		appG.Response(http.StatusNotFound, "没有该活动", nil)
+		return
+	}
+	//判断活动是否开启
+	//获取当前时间戳
+	timestamp := time.Now().Unix()
 
-	var data []map[string]interface{}
+	if int(timestamp) < activityData.StartTime {
+		appG.Response(http.StatusBadRequest, "活动未开启", nil)
+		return
+	}
 
-	appG.Response(http.StatusOK, "成功", data)
+	//var data []map[string]interface{}
+
+	appG.Response(http.StatusOK, "成功", body)
 }
--- application/server/model/init.go
+++ application/server/model/init.go
@@ -5,14 +5,19 @@ import (
 	"gorm.io/gorm"
 )
 
+var (
+	DB  *gorm.DB
+	err error
+)
+
 func init() {
 	// 参考 https://github.com/go-sql-driver/mysql#dsn-data-source-name 获取详情
 	dsn := "root:@tcp(127.0.0.1:3306)/fabirc_fang?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic("failed to connect database")
 	}
 
-	db.AutoMigrate(&Activity{}, &Goods{}, &ActivityGoods{}, &Order{})
+	DB.AutoMigrate(&Activity{}, &Goods{}, &ActivityGoods{}, &Order{})
 }
