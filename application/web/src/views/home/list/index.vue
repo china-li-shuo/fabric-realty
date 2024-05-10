eae8e2fa37eb59be433391d399274f63f4a866fa
china-li-shuo <676050059@qq.com>
2024-05-10 15:39:10 +0800

    home

--- application/web/src/views/home/list/index.vue
+++ application/web/src/views/home/list/index.vue
@@ -1,24 +1,55 @@
 <template>
-   <div>首页</div>
+   <div id="main" style="width: 600px; height: 400px;"></div>
 </template>
 
 <script>
-    import { mapGetters } from 'vuex'
-    import { queryAccountList } from '@/api/account'
-    import { queryRealEstateList } from '@/api/realEstate'
-    import { createSelling } from '@/api/selling'
-    import { createDonating } from '@/api/donating'
+   import * as echarts from 'echarts';
 
-    export default {
-        name: 'Home',
-        data() {
-           return {}
-        },
-        methods: {
-        }
-    }
-
+   export default {
+      data() {
+         return {
+            myChart: null,
+            option: {
+               title: {
+                  text: '房产交易图',
+                  subtext: '2024.5.10',
+                  left: 'center'
+               },
+               tooltip: {
+                  trigger: 'item'
+               },
+               legend: {
+                  orient: 'vertical',
+                  left: 'left'
+               },
+               series: [
+                  {
+                     name: 'Access From',
+                     type: 'pie',
+                     radius: '50%',
+                     data: [
+                        { value: 1, name: '已出售' },
+                        { value: 2, name: '未出售' },
+                     ],
+                     emphasis: {
+                        itemStyle: {
+                           shadowBlur: 10,
+                           shadowOffsetX: 0,
+                           shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                     }
+                  }
+               ]
+            }
+         };
+      },
+      mounted() {
+         this.myChart = echarts.init(document.getElementById('main'));
+         this.myChart.setOption(this.option);
+      }
+   };
 </script>
 
-<style>
+<style scoped>
+   /* 根据需要添加样式 */
 </style>
